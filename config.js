6dc28adff6b81f48b8d5375ca504030833086433
Jacob Silver <jacobsilver2@mac.com>
2020-05-09 23:47:43 -0400

    changed endpoint

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 //this is client side config ony. don't put anything in here that
 // shouldn't be public
 
-export const endpoint = `http://localhost:4000`;
+// export const endpoint = `http://localhost:4000`;
 // export const prodEndpoint = `https://sickfits-yoga-prod.herokuapp.com/`;
+export const endpoint = "157.245.87.198";
 export const perPage = 50;
